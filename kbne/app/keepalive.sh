07a1855d1e37f6171524e7b9bdff0fe49222077b
justlagom <93416015+justlagom@users.noreply.github.com>
2025-12-08 14:12:30 +0800

    Update keepalive.sh

--- kbne/app/keepalive.sh
+++ kbne/app/keepalive.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
-status=$(curl -o /dev/null -s -w "%{http_code}" https://DOMAIN)
-echo `date "+%Y-%m-%d %H:%M:%S"` - Request: https://DOMAIN, Response: $status > /tmp/keepalive.log
+# 将占位符改为 DOMAIN_PLACEHOLDER，以便与 entrypoint.sh 中的 sed 命令匹配
+status=$(curl -o /dev/null -s -w "%{http_code}" https://DOMAIN_PLACEHOLDER)
+echo `date "+%Y-%m-%d %H:%M:%S"` - Request: https://DOMAIN_PLACEHOLDER, Response: $status > /tmp/keepalive.log
